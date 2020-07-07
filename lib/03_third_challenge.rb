3573d5dd9ecfc50d3337de06937d8f64e3508e75
Junko Tahara <junkotahara911@gmail.com>
2020-07-07 03:01:10 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -8,22 +8,31 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        
+        :name => "Lord Montague",
+        :age => "53"
         },
       :hero => {
         #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :heroine => {
         #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :heroine_friends => []
    }
