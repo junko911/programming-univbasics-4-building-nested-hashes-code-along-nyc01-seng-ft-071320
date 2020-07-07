44b52a4f4bf28f7af7d8248d2961b7183f0f2f5d
Junko Tahara <junkotahara911@gmail.com>
2020-07-07 03:05:24 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -25,7 +25,7 @@ def fourth_challenge
         :attitude => "confused"},
         {:name => "Nurse",
         :age => "44",
-        :attitude => ""}
+        :attitude => "worried"}
       ]
    }
 }
